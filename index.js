607d6c80a87ed1287f47f45e5240700e6e3f6a69
VIGNESH S <vickysvignesh3161@gmail.com>
2025-10-03 13:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import { sendEmail } from "./server.js";
 
-dotenv.config();
 const app = express();
 
 // Middleware
@@ -39,3 +37,4 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
